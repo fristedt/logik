de378b4e8536b48a1d22c53c0ce00bcca5c2b979
Anton Lindqvist <anton@qvister.se>
2013-11-01 13:55:26 +0100

    Added double negation and fixed var name.

--- lab1.pl
+++ lab1.pl
@@ -12,50 +12,55 @@ valid_proof(Prems, Goal, Proof) :-
 % Check if proof is valid.
 valid_proof(_, _, [], _) :- !.
 % Premise.
-valid_proof(Prems, Goal, [[LineNumber, Predicate, premise]|T], Previously) :-
+valid_proof(Prems, Goal, [[L, Predicate, premise]|T], Previously) :-
   is_premise(Previously),
   valid_premise(Predicate, Prems), !,
-  valid_proof(Prems, Goal, T, [[LineNumber, Predicate, premise]|Previously]).
+  valid_proof(Prems, Goal, T, [[L, Predicate, premise]|Previously]).
 % Copy.
-valid_proof(Prems, Goal, [[LineNumber, Y, copy(X)]|T], Previously) :-
+valid_proof(Prems, Goal, [[L, Y, copy(X)]|T], Previously) :-
   lookup_line(X, Previously, Y), !,
-  valid_proof(Prems, Goal, T, [[LineNumber, Y, copy(X)]|Previously]).
+  valid_proof(Prems, Goal, T, [[L, Y, copy(X)]|Previously]).
 % And introduction.
-valid_proof(Prems, Goal, [[LineNumber, and(A, B), andint(X, Y)]|T], Previously) :-
+valid_proof(Prems, Goal, [[L, and(A, B), andint(X, Y)]|T], Previously) :-
   lookup_line(X, Previously, A),
   lookup_line(Y, Previously, B), !,
-  valid_proof(Prems, Goal, T, [[LineNumber, and(A, B), andint(X, Y)]|Previously]).
+  valid_proof(Prems, Goal, T, [[L, and(A, B), andint(X, Y)]|Previously]).
 % And elimination 1.
-valid_proof(Prems, Goal, [[LineNumber, Y, andel1(X)]|T], Previously) :-
+valid_proof(Prems, Goal, [[L, Y, andel1(X)]|T], Previously) :-
   lookup_line(X, Previously, and(Y, _)), !,
-  valid_proof(Prems, Goal, T, [[LineNumber, Y, andel1(X)]|Previously]).
+  valid_proof(Prems, Goal, T, [[L, Y, andel1(X)]|Previously]).
 % And elimination 2.
-valid_proof(Prems, Goal, [[LineNumber, Y, andel2(X)]|T], Previously) :-
+valid_proof(Prems, Goal, [[L, Y, andel2(X)]|T], Previously) :-
   lookup_line(X, Previously, and(_, Y)), !,
-  valid_proof(Prems, Goal, T, [[LineNumber, Y, andel2(X)]|Previously]).
+  valid_proof(Prems, Goal, T, [[L, Y, andel2(X)]|Previously]).
 % Or introduction 1.
-valid_proof(Prems, Goal, [[LineNumber, or(Y, Z), orint1(X)]|T], Previously) :-
+valid_proof(Prems, Goal, [[L, or(Y, Z), orint1(X)]|T], Previously) :-
   lookup_line(X, Previously, Y), !,
-  valid_proof(Prems, Goal, T, [[LineNumber, or(Y, Z), orint1(X)]|Previously]).
+  valid_proof(Prems, Goal, T, [[L, or(Y, Z), orint1(X)]|Previously]).
 % Or introduction 2.
-valid_proof(Prems, Goal, [[LineNumber, or(Z, Y), orint2(X)]|T], Previously) :-
+valid_proof(Prems, Goal, [[L, or(Z, Y), orint2(X)]|T], Previously) :-
   lookup_line(X, Previously, Y), !,
-  valid_proof(Prems, Goal, T, [[LineNumber, or(Z, Y), orint2(X)]|Previously]).
+  valid_proof(Prems, Goal, T, [[L, or(Z, Y), orint2(X)]|Previously]).
 % Double negation elimination.
-valid_proof(Prems, Goal, [[LineNumber, neg(neg(Y)), negnegel(X)]|T], Previously) :-
+valid_proof(Prems, Goal, [[L, neg(neg(Y)), negnegel(X)]|T], Previously) :-
   lookup_line(X, Previously, Y), !,
-  valid_proof(Prems, Goal, T, [[LineNumber, neg(neg(Y)), negnegel(X)]|Previously]).
+  valid_proof(Prems, Goal, T, [[L, neg(neg(Y)), negnegel(X)]|Previously]).
+% Double negation elimination.
+valid_proof(Prems, Goal, [[L, B, negnegel(X)]|T], Previously) :-
+  lookup_line(X, Previously, neg(neg(B))),
+  valid_proof(Prems, Goal, T, [[L, B, negnegel(X)]|Previously]).
+
 % 2. p -> neg(p) A
 % 3. p           Z
 % 4. neg(p)      B
 % Implication elimination.
-valid_proof(Prems, Goal, [[LineNumber, B, impel(X, Y)]|T], Previously) :-
+valid_proof(Prems, Goal, [[L, B, impel(X, Y)]|T], Previously) :-
   lookup_line(X, Previously, Z),
   lookup_line(Y, Previously, imp(Z, B)), !,
-  valid_proof(Prems, Goal, T, [[LineNumber, B, impel(X, Y)]|Previously]).
+  valid_proof(Prems, Goal, T, [[L, B, impel(X, Y)]|Previously]).
 
-% valid_proof(Prems, Goal, [[LineNumber, Predicate, assumption]|T], Previously) :-
-%   valid_proof(Prems, Goal, T, [[LineNumber, Predicate, assumption]|Previously]).
+% valid_proof(Prems, Goal, [[L, Predicate, assumption]|T], Previously) :-
+%   valid_proof(Prems, Goal, T, [[L, Predicate, assumption]|Previously]).
 
 % Check if line is valid.
 valid_line([_, P, premise], Prems) :- 
