416395997317f773a1a69b43d9d33f0d9bfb76e4
Anton Lindqvist <anton@qvister.se>
2013-11-01 14:05:32 +0100

    Added MT.

--- lab1.pl
+++ lab1.pl
@@ -50,6 +50,12 @@ valid_proof(Prems, Goal, [[L, B, negnegel(X)]|T], Previously) :-
   lookup_line(X, Previously, neg(neg(B))),
   valid_proof(Prems, Goal, T, [[L, B, negnegel(X)]|Previously]).
 
+% MT
+valid_proof(Prems, Goal, [[L, neg(B), mt(X, Y)]|T], Previously) :-
+  lookup_line(X, Previously, imp(B, A)),
+  lookup_line(Y, Previously, neg(A)),
+  valid_proof(Prems, Goal, T, [[L, neg(B), mt(X, Y)]|Previously]).
+
 % 2. p -> neg(p) A
 % 3. p           Z
 % 4. neg(p)      B
